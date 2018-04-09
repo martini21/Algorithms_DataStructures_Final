b9a98ef0865ee9765af36712339167b913a46180
elvinaslukasevicius <elvinas.lukasevicius@gmail.com>
2018-04-09 11:28:19 +0200

    BubbleSort and QuickSortBubbleSort and QuickSort

---
+++ src/QuickSortTest.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class QuickSortTest{
+    public static void main(String[] args){
+        try{
+            new Legocsv().readFile();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        Quicksort_Algorithm quicksort = new Quicksort_Algorithm(Legocsv.itemsArray);
+        quicksort.sort();
+        quicksort.printArray();
+    }
+    /*
+    @org.junit.Before
+    public void Before()
+    {
+        this.list = new LinkedList();
+    }
+
+    @org.junit.Test
+    public void push()
+    {
+        this.list.push(5);
+        assertEquals(5, this.list.head.data);
+        this.list.push(3);
+        assertEquals(3, this.list.head.data);
+    }
+
+    @org.junit.Test
+    public void insertAfter()
+    {
+        this.list.push(5);
+        this.list.insertAfter(this.list.head, 3);
+        assertEquals(3, this.list.head.next.data);
+    }
+
+    @org.junit.Test
+    public void append()
+    {
+        this.list.append(2);
+        assertEquals(2, this.list.head.data);
+        this.list.append(3);
+        assertEquals(3, this.list.head.next.data);
+    }*/
+}
